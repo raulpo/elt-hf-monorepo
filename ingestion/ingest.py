df1300ab600a2c3a982bf89f6d61f19961b036eb
Raul Paiva <raulpaiva@gmail.com>
2024-10-10 20:01:32 -0300

    added dlt, removed airbyte from ingestion project

--- ingestion/ingest.py
+++ ingestion/ingest.py
@@ -1,45 +1,26 @@
+import dlt
 import duckdb
 
-import airbyte as ab
-from airbyte.caches import PostgresCache
 
-from os import getenv
-from dotenv import load_dotenv
+def run_ingestion():
+    query = '''
+        SELECT *
+        FROM 'hf://datasets/Linkseed/hacker_news_with_comments/test.csv';
+    '''
+    df = duckdb.sql(query).to_arrow_table()
 
-load_dotenv()
+    pipeline = dlt.pipeline(
+        pipeline_name="hackernews_pipeline",
+        destination="postgres",
+        dataset_name="raw"
+    )
 
-hackernews_dataset = duckdb.sql("SELECT * FROM 'hf://datasets/Linkseed/hacker_news_with_comments/test.csv';").df().to_csv("hackernews_dataset.csv")
-
-source: ab.Source = ab.get_source("source-file")
-
-# Configure the source
-source.set_config(
-    config={
-        "dataset_name": "raw_hacker_news_with_comments",
-        "format": "csv",  # Adjust this to get a larger or smaller dataset
-        "url": "hackernews_dataset.csv",
-        "provider": { 
-            "title" : "Local Filesystem (limited)",
-            "storage": "local"
-        },        
-    },
-)
-
-# Verify the config and creds by running `check`:
-source.check()
+    pipeline.run(
+        df,
+        table_name="raw_hacker_news_with_comments",
+        write_disposition="replace"
+    )
 
 
-#Define a Postgres Cache and pass the necessary configuration
-pg_cache = PostgresCache(    
-      host=getenv('PG_HOST'),
-      port=getenv('PG_PORT'),
-      username=getenv('PG_USERNAME'),
-      password=getenv('PG_PASSWORD'),
-      database=getenv('PG_DATABASE'),
-      schema_name=getenv('PG_SCHEMA_RAW')
-)
-
-# Select all of the source's streams and read data into the previously defined Postgres cache:
-source.select_all_streams()
-
-read_result: ab.ReadResult = source.read(cache=pg_cache)
+if __name__ == "__main__":
+    run_ingestion()
